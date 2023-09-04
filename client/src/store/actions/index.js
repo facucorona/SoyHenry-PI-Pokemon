5a55ec74817663e6a7a426de1ee0a02465984658
Facu Corona <coronafacundo@gmail.com>
2023-09-04 00:25:08 -0300

    console.log en getPokemons

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -2,7 +2,7 @@ export const GET_POKEMONS = "GET_POKEMONS";
 export const GET_DETAILS = "GET_DETAILS";
 export const CLEAN_STATE = "CLEAN_STATE";
 export const SEARCH = "SEARCH";
-export const GET_TYPES = "GET_TYPES"; 
+export const GET_TYPES = "GET_TYPES";
 export const RESET_STATE = "RESET_STATE";
 export const FILTER_STATE = "FILTER_STATE";
 export const FILTER_STATE_ORIGIN = "FILTER_STATE_ORIGIN";
@@ -21,110 +21,111 @@ export function deletePokemon(array) {
     }
 }
 
-export function unmountBackup(backupArray){
+export function unmountBackup(backupArray) {
     return async (dispatch) => {
         dispatch({
             type: UNMOUNT_BACKUP,
             payload: backupArray,
-        })        
+        })
     }
 }
 
-export function resetState(resetArray){
+export function resetState(resetArray) {
     return async (dispatch) => {
         dispatch({
             type: RESET_STATE,
             payload: resetArray,
-        })        
+        })
     }
 }
 
-export function setPageFalse(){
+export function setPageFalse() {
     return async (dispatch) => {
         dispatch({
             type: SET_PAGE_FALSE,
             payload: false,
-        })        
+        })
     }
 }
 
-export function filterState(filteredArray){
+export function filterState(filteredArray) {
     // console.log(filteredArray)
     return async (dispatch) => {
         dispatch({
             type: FILTER_STATE,
             payload: filteredArray,
-        })        
+        })
     }
 }
 
-export function filterStateOrigin(filter, array){
+export function filterStateOrigin(filter, array) {
     // console.log(filter)
     return async (dispatch) => {
         dispatch({
             type: FILTER_STATE_ORIGIN,
-            payload:{ filter, array}
-        })        
+            payload: { filter, array }
+        })
     }
 }
 
-export function orderState(orderedArray){
+export function orderState(orderedArray) {
     // console.log("orderedArray .,.,.,.,.,.,.,.,.")
     // console.log(orderedArray)
     return async (dispatch) => {
         dispatch({
             type: ORDER_STATE,
             payload: orderedArray,
-        })        
+        })
     }
 }
 
-export function getPokemons(){
+export function getPokemons() {
     return async (dispatch) => {
+        console.log('process.env.REACT_APP_HOST_BACK: ', process.env.REACT_APP_HOST_BACK)
         await fetch(`${process.env.REACT_APP_HOST_BACK}/pokemons`)
-        .then(p=>p.json())
-        .then((arrayFetch)=>{
-            dispatch({
-                type: GET_POKEMONS,
-                payload: arrayFetch,
+            .then(p => p.json())
+            .then((arrayFetch) => {
+                dispatch({
+                    type: GET_POKEMONS,
+                    payload: arrayFetch,
+                })
+
             })
 
-        })
-        
     }
 }
 
-export function getTypes(){
+export function getTypes() {
     return async (dispatch) => {
         await fetch(`${process.env.REACT_APP_HOST_BACK}/types`)
-        .then(p=>p.json())
-        .then((arrayFetch)=>{
-            dispatch({
-                type: GET_TYPES,
-                payload: arrayFetch,
+            .then(p => p.json())
+            .then((arrayFetch) => {
+                dispatch({
+                    type: GET_TYPES,
+                    payload: arrayFetch,
+                })
+
             })
 
-        })
-        
     }
 }
 
-export function getDetails(id){
+export function getDetails(id) {
     return async (dispatch) => {
         await fetch(`${process.env.REACT_APP_HOST_BACK}/pokemons/${id}`)
-        .then(p=>p.json())
-        .then((arrayFetch)=>{
-            dispatch({
-                type: GET_DETAILS,
-                payload: arrayFetch,
+            .then(p => p.json())
+            .then((arrayFetch) => {
+                dispatch({
+                    type: GET_DETAILS,
+                    payload: arrayFetch,
+                })
+
             })
 
-        })
-        
     }
 }
-export function cleanDetails(){
-    return (dispatch)=>{
+export function cleanDetails() {
+    return (dispatch) => {
         dispatch({
             type: CLEAN_DETAILS,
             payload: [],
@@ -132,8 +133,8 @@ export function cleanDetails(){
     }
 }
 
-export function cleanState(){
-    return (dispatch)=>{
+export function cleanState() {
+    return (dispatch) => {
         dispatch({
             type: CLEAN_STATE,
             payload: [],
@@ -141,17 +142,17 @@ export function cleanState(){
     }
 }
 
-export function search(pk){
+export function search(pk) {
     return async (dispatch) => {
         await fetch(`${process.env.REACT_APP_HOST_BACK}/pokemons/?name=${pk}`)
-        .then(p=>p.json())
-        .then((arrayFetch)=>{
-            dispatch({
-                type: SEARCH,
-                payload: arrayFetch,
+            .then(p => p.json())
+            .then((arrayFetch) => {
+                dispatch({
+                    type: SEARCH,
+                    payload: arrayFetch,
+                })
+
             })
 
-        })
-        
     }
 }
