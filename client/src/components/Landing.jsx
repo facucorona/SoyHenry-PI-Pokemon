88c42ae2511ae38d0e67ddff306b381aa4864ef4
FacuCorona <coronafacundo@gmail.com>
2025-10-13 01:13:03 -0300

    test

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import style from './styles/Landing.module.css'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import style from "./styles/Landing.module.css";
 
 export function Landing() {
-    return (
-        <div key={"landingContainer"} className={style.container}>
-            <NavLink to={'/home'}>
-                <button title={"enter button"} className={style.button}>Enter Pokédex</button>
-            </NavLink>
-            <h6 className={style.press}>press the button</h6>
-        </div >
-    )
+  return (
+    <div key={"landingContainer"} className={style.container}>
+      <NavLink to={"/home"}>
+        <button title={"enter button"} className={style.button}>
+          Enter Pokédex NOW
+        </button>
+      </NavLink>
+      <h6 className={style.press}>press the button</h6>
+    </div>
+  );
 }
 
-export default Landing
+export default Landing;
--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -13,146 +13,141 @@ export const CLEAN_DETAILS = "CLEAN_DETAILS";
 export const DELETE_POKEMON = "DELETE_POKEMON";
 
 export function deletePokemon(array) {
-    return (dispatch) => {
-        dispatch({
-            type: DELETE_POKEMON,
-            payload: array,
-        })
-    }
+  return (dispatch) => {
+    dispatch({
+      type: DELETE_POKEMON,
+      payload: array,
+    });
+  };
 }
 
 export function unmountBackup(backupArray) {
-    return async (dispatch) => {
-        dispatch({
-            type: UNMOUNT_BACKUP,
-            payload: backupArray,
-        })
-    }
+  return async (dispatch) => {
+    dispatch({
+      type: UNMOUNT_BACKUP,
+      payload: backupArray,
+    });
+  };
 }
 
 export function resetState(resetArray) {
-    return async (dispatch) => {
-        dispatch({
-            type: RESET_STATE,
-            payload: resetArray,
-        })
-    }
+  return async (dispatch) => {
+    dispatch({
+      type: RESET_STATE,
+      payload: resetArray,
+    });
+  };
 }
 
 export function setPageFalse() {
-    return async (dispatch) => {
-        dispatch({
-            type: SET_PAGE_FALSE,
-            payload: false,
-        })
-    }
+  return async (dispatch) => {
+    dispatch({
+      type: SET_PAGE_FALSE,
+      payload: false,
+    });
+  };
 }
 
 export function filterState(filteredArray) {
-    // console.log(filteredArray)
-    return async (dispatch) => {
-        dispatch({
-            type: FILTER_STATE,
-            payload: filteredArray,
-        })
-    }
+  // console.log(filteredArray)
+  return async (dispatch) => {
+    dispatch({
+      type: FILTER_STATE,
+      payload: filteredArray,
+    });
+  };
 }
 
 export function filterStateOrigin(filter, array) {
-    // console.log(filter)
-    return async (dispatch) => {
-        dispatch({
-            type: FILTER_STATE_ORIGIN,
-            payload: { filter, array }
-        })
-    }
+  // console.log(filter)
+  return async (dispatch) => {
+    dispatch({
+      type: FILTER_STATE_ORIGIN,
+      payload: { filter, array },
+    });
+  };
 }
 
 export function orderState(orderedArray) {
-    // console.log("orderedArray .,.,.,.,.,.,.,.,.")
-    // console.log(orderedArray)
-    return async (dispatch) => {
-        dispatch({
-            type: ORDER_STATE,
-            payload: orderedArray,
-        })
-    }
+  // console.log("orderedArray .,.,.,.,.,.,.,.,.")
+  // console.log(orderedArray)
+  return async (dispatch) => {
+    dispatch({
+      type: ORDER_STATE,
+      payload: orderedArray,
+    });
+  };
 }
 
 export function getPokemons() {
-    return async (dispatch) => {
-        // console.log('process.env.REACT_APP_HOST_BACK: ', process.env.REACT_APP_HOST_BACK)
-        await fetch(`${process.env.REACT_APP_HOST_BACK}/pokemons`)
-            .then(p => p.json())
-            .then((arrayFetch) => {
-                dispatch({
-                    type: GET_POKEMONS,
-                    payload: arrayFetch,
-                })
-
-            })
-
-    }
+  return async (dispatch) => {
+    console.log(
+      "process.env.REACT_APP_HOST_BACK: ",
+      process.env.REACT_APP_HOST_BACK
+    );
+    await fetch(`${process.env.REACT_APP_HOST_BACK}/pokemons`)
+      .then((p) => p.json())
+      .then((arrayFetch) => {
+        dispatch({
+          type: GET_POKEMONS,
+          payload: arrayFetch,
+        });
+      });
+  };
 }
 
 export function getTypes() {
-    return async (dispatch) => {
-        await fetch(`${process.env.REACT_APP_HOST_BACK}/types`)
-            .then(p => p.json())
-            .then((arrayFetch) => {
-                dispatch({
-                    type: GET_TYPES,
-                    payload: arrayFetch,
-                })
-
-            })
-
-    }
+  return async (dispatch) => {
+    await fetch(`${process.env.REACT_APP_HOST_BACK}/types`)
+      .then((p) => p.json())
+      .then((arrayFetch) => {
+        dispatch({
+          type: GET_TYPES,
+          payload: arrayFetch,
+        });
+      });
+  };
 }
 
 export function getDetails(id) {
-    return async (dispatch) => {
-        await fetch(`${process.env.REACT_APP_HOST_BACK}/pokemons/${id}`)
-            .then(p => p.json())
-            .then((arrayFetch) => {
-                dispatch({
-                    type: GET_DETAILS,
-                    payload: arrayFetch,
-                })
-
-            })
-
-    }
+  return async (dispatch) => {
+    await fetch(`${process.env.REACT_APP_HOST_BACK}/pokemons/${id}`)
+      .then((p) => p.json())
+      .then((arrayFetch) => {
+        dispatch({
+          type: GET_DETAILS,
+          payload: arrayFetch,
+        });
+      });
+  };
 }
 export function cleanDetails() {
-    return (dispatch) => {
-        dispatch({
-            type: CLEAN_DETAILS,
-            payload: [],
-        })
-    }
+  return (dispatch) => {
+    dispatch({
+      type: CLEAN_DETAILS,
+      payload: [],
+    });
+  };
 }
 
 export function cleanState() {
-    return (dispatch) => {
-        dispatch({
-            type: CLEAN_STATE,
-            payload: [],
-        })
-    }
+  return (dispatch) => {
+    dispatch({
+      type: CLEAN_STATE,
+      payload: [],
+    });
+  };
 }
 
 export function search(pk) {
-    return async (dispatch) => {
-        await fetch(`${process.env.REACT_APP_HOST_BACK}/pokemons/?name=${pk}`)
-            .then(p => p.json())
-            .then((arrayFetch) => {
-                dispatch({
-                    type: SEARCH,
-                    payload: arrayFetch,
-                })
-
-            })
-
-    }
+  return async (dispatch) => {
+    await fetch(`${process.env.REACT_APP_HOST_BACK}/pokemons/?name=${pk}`)
+      .then((p) => p.json())
+      .then((arrayFetch) => {
+        dispatch({
+          type: SEARCH,
+          payload: arrayFetch,
+        });
+      });
+  };
 }
